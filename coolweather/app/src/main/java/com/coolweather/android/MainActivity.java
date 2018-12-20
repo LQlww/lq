64df03b1e65267995e5316530daca5028cc50b19
lww <1299632470@qq.com>
2018-12-20 15:56:23 +0800

    天气预报

---
+++ coolweather/app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,26 @@
+package com.coolweather.android;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //从SharedPreferences文件中读取数据
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //不为空就说明之前已经请求过天气数据，直接跳到WeatherActivity
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
---
+++ coolweather/app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+//和服务器进行交互
+public class HttpUtil {
+    //发送一条HTTP请求调用sendOkHttpRequest()方法
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
